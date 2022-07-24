03b522ac012457729e33670f6c95751b6738f96d
Halleluyah <adesuyihalleluyah@gmail.com>
2022-07-24 01:06:42 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-// import Modal from './Modal/Modal'
+import searchImage from './search.png' 
 
 function App() {
   const [searchText, setSearchText] = useState('random')
   const [imagesRender, setImagesRender] = useState([])
-  const [modal, setModal] = useState(false)
-  const [modalImage, setModalImage] = useState({})
+  /* const [modal, setModal] = useState(false) */
 
   const clientId = 'bHQPxBXq-GgLBzTlaxQY28uz_s4PP0Z15aTTjYFzzxU'
   const url = `https://api.unsplash.com/search/photos?page=1&per_page=20&query=${searchText}&client_id=${clientId}`
@@ -16,32 +15,30 @@ function App() {
       .then(res => res.json())
       .then(data => {
         console.log(data.results)
-        setImagesRender(data.results)})
+        setImagesRender(data.results)
+      })
   }
 
   useEffect(imagesFetchRequest, [])
-  
+
   function handleSearch() {
     imagesFetchRequest()
-  } 
+  }
 
-  function toggleModal() {
-    return (
-      setModal(!modal)
-    )
+  function openModal(id) {
+    document.getElementById(id).style.display = 'block'
+
   };
 
-  function handleModalImage() {
-    setModalImage(() => {
-        console.log('displayed')
-    })
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none'
   }
 
-  if(modal) {
-    document.body.classList.add('active-modal')
-  } else {
-    document.body.classList.remove('active-modal')
-  }
+  /*  if(modal) {
+      document.body.classList.add('active-modal')
+    } else {
+      document.body.classList.remove('active-modal')
+    } */
 
   /*{modal && (
     <div className="modal">
@@ -60,60 +57,60 @@ function App() {
     <div className='app'>
       <div className='top'>
         <h1>Responsive Image-Gallery App</h1>
+
+        <section className='search-bar'>
+          <input
+            type='text'
+            name='images'
+            placeholder='Enter keywords to explore images...'
+            onChange={(e) => setSearchText(e.target.value)}
+            autocomplete='off'
+            className='search-input'
+          />
+          <img 
+            src={searchImage}
+            alt='search'
+            onClick={handleSearch}
+            className='search-lens' />
+        </section>
         
-        <input 
-          type='text'
-          name='images'
-          placeholder='Type in keywords to search for images...'
-          onChange={(e) => setSearchText(e.target.value)}
-          autocomplete= 'off'
-        />
-        
-        <button 
-          type='submit' 
-          onClick={handleSearch}
-          className='searchBtn'
-        >
-            Search
-        </button>
       </div>
-        
+
       <div className='image-container'>
         {imagesRender.map((image) => {
           return (
-            <>
-              <div className='image'>
+            <section>
+              <div className='vignette-hover-effect' key={image.id}>
                 <img
-                  key={image.id}
                   src={image.urls.thumb}
                   alt={image.alt_description}
-                  onClick={() => toggleModal()}
+                  onClick={() => openModal(image.id)}
+                  className='image'
                 />
               </div>
 
-              {modal && (
-                <div className="modal">
-                  <div onClick={toggleModal} className="overlay"></div>
-                  <div className="modal-content">
-                    <img
-                      key={image.id}
-                      src={image.urls.thumb}
-                      alt={image.alt_description}
-                    />
-                    
-                    <button className="close-modal" onClick={toggleModal}>
-                      &times;
-                    </button>
-                  </div>
+              <div className="modal" id={image.id}>
+                <div onClick={closeModal} className="overlay"></div>
+                <div className="modal-content">
+                  <img
+                    src={image.urls.thumb}
+                    alt={image.alt_description}
+                  />
+
+                  <button className="close-modal" onClick={() => closeModal(image.id)}>
+                    &times;
+                  </button>
                 </div>
-              )}
-            </>
-          )}
+              </div>
+
+            </section>
+          )
+        }
         )}
       </div>
     </div>
 
-    
+
   )
 }
 
