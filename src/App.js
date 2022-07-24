d603dd47892da5d383d90536784b95abf3faf3d3
Halleluyah <adesuyihalleluyah@gmail.com>
2022-07-25 00:51:40 +0100

    hover-effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import searchImage from './search.png' 
+import searchImage from './search.png'
 
 function App() {
   const [searchText, setSearchText] = useState('random')
   const [imagesRender, setImagesRender] = useState([])
-  /* const [modal, setModal] = useState(false) */
 
   const clientId = 'bHQPxBXq-GgLBzTlaxQY28uz_s4PP0Z15aTTjYFzzxU'
   const url = `https://api.unsplash.com/search/photos?page=1&per_page=20&query=${searchText}&client_id=${clientId}`
@@ -27,31 +26,11 @@ function App() {
 
   function openModal(id) {
     document.getElementById(id).style.display = 'block'
-
-  };
+  }
 
   function closeModal(id) {
     document.getElementById(id).style.display = 'none'
   }
-
-  /*  if(modal) {
-      document.body.classList.add('active-modal')
-    } else {
-      document.body.classList.remove('active-modal')
-    } */
-
-  /*{modal && (
-    <div className="modal">
-      <div onClick={toggleModal} className="overlay"></div>
-      <div className="modal-content">
-        <h2>Hello Modal</h2>
-        <p> Lorem </p>
-        <button className="close-modal" onClick={toggleModal}>
-          &times;
-        </button>
-      </div>
-    </div>
-  )}*/
 
   return (
     <div className='app'>
@@ -64,45 +43,44 @@ function App() {
             name='images'
             placeholder='Enter keywords to explore images...'
             onChange={(e) => setSearchText(e.target.value)}
-            autocomplete='off'
+            autoComplete='off'
             className='search-input'
           />
-          <img 
+          <img
             src={searchImage}
             alt='search'
             onClick={handleSearch}
             className='search-lens' />
         </section>
-        
       </div>
 
       <div className='image-container'>
         {imagesRender.map((image) => {
           return (
-            <section>
-              <div className='vignette-hover-effect' key={image.id}>
+            <section key={image.id}>
+              <div className='img-wrapper'>
                 <img
                   src={image.urls.thumb}
                   alt={image.alt_description}
                   onClick={() => openModal(image.id)}
                   className='image'
                 />
+                <div className="img-overlay">Yadayada</div>
               </div>
 
               <div className="modal" id={image.id}>
                 <div onClick={closeModal} className="overlay"></div>
-                <div className="modal-content">
-                  <img
-                    src={image.urls.thumb}
-                    alt={image.alt_description}
-                  />
+                  <div className="modal-content">
+                    <img
+                      src={image.urls.thumb}
+                      alt={image.alt_description}
+                    />
 
-                  <button className="close-modal" onClick={() => closeModal(image.id)}>
-                    &times;
-                  </button>
+                    <button className="close-modal" onClick={() => closeModal(image.id)}>
+                      &times;
+                    </button>
+                  </div>
                 </div>
-              </div>
-
             </section>
           )
         }
